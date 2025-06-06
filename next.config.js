f3ea4278dc3d07cfe26bf6c4eb6a7fe08833a251
benchmarkaibiz <geoffrey.rabussier@rgpa.fr>
2025-06-06 22:53:36 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/auth/login',
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
